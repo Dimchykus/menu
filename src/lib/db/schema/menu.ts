1eb17c109f63af72c04198b4fd1bf373911b843c
Dima Pyshka <dimhukys@gmail.com>
2025-05-20 19:25:03 +0300

    menu schema

---
+++ src/lib/db/schema/menu.ts
@@ -0,0 +1,46 @@
+import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { userTable } from "./user";
+
+export const restaurantTable = pgTable("restaurants", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => userTable.id),
+  name: text("name").notNull(),
+  description: text("description"),
+  address: text("address"),
+  phone: text("phone"),
+  createdAt: timestamp("created_at").defaultNow(),
+});
+
+export const menuTable = pgTable("menus", {
+  id: serial("id").primaryKey(),
+  restaurantId: integer("restaurant_id")
+    .notNull()
+    .references(() => restaurantTable.id),
+  name: text("name").notNull(),
+  description: text("description"),
+});
+
+export const menuCategoryTable = pgTable("menu_categories", {
+  id: serial("id").primaryKey(),
+  menuId: integer("menu_id")
+    .notNull()
+    .references(() => menuTable.id),
+  name: text("name").notNull(),
+  description: text("description"),
+});
+
+export const dishTable = pgTable("dishes", {
+  id: serial("id").primaryKey(),
+  categoryId: integer("category_id")
+    .notNull()
+    .references(() => menuCategoryTable.id),
+  name: text("name").notNull(),
+  description: text("description"),
+  image: text("image"),
+  price: integer("price"),
+  weight: integer("weight"),
+  weight_type: text("weight_type"),
+  // ingredients
+});
